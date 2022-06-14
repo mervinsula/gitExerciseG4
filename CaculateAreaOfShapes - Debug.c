3c6602fb3e4bf3d701e27294588dee04746de3e3
Michaela Cabauatan <michaelarose.cabauatan@gmail.com>
2022-06-14 15:34:41 +0800

    Edit Line102
    debugged line 102 to get area of square
    from: Int side, area;
    to: int side, area;

--- CaculateAreaOfShapes - Debug.c
+++ CaculateAreaOfShapes - Debug.c
@@ -99,7 +99,7 @@ void calAreaOfTriangle(){
 * This function calculates the area of a square.
 */
 void calAreaOfSquare(){
-   Int side, area;
+   int side, area;
  
    printf("\nEnter the Length of Side : ");
    scanf("%d", &side);
